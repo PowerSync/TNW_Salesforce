ed7837d50e196726138c0b748373cf286c20fe34
eermolaev <eermolaev@yandex.ru>
2018-10-16 19:49:12 +0400

    PCMII-549 realtime sync

---
+++ Synchronize/Entity/DevideEntityByWebsiteOrg.php
@@ -0,0 +1,52 @@
+<?php
+namespace TNW\Salesforce\Synchronize\Entity;
+
+abstract class DevideEntityByWebsiteOrg
+{
+
+    /** @var \TNW\Salesforce\Model\Config  */
+    protected $config;
+
+    /**
+     * Entity constructor.
+     * @param \TNW\Salesforce\Model\Config $config
+     */
+    public function __construct(
+        \TNW\Salesforce\Model\Config $config
+    ) {
+        $this->config = $config;
+    }
+
+    /**
+     * @param $entity
+     * @return mixed
+     */
+    abstract public function getEntityWebsiteIds($entity);
+
+    /**
+     * @param $ids
+     * @return mixed
+     */
+    abstract public function loadEntities($ids);
+
+    /**
+     * @param array $entities
+     * @return array
+     */
+    public function process(array $entities)
+    {
+        $groupedWebsites = $this->config->getWebsitesGrouppedByOrg();
+
+        $entities = $this->loadEntities($entities);
+
+        $entitiesByWebsites = [];
+        foreach ($entities as $entity) {
+            foreach ($this->getEntityWebsiteIds($entity) as $entityWebsiteId) {
+                $generalWebsiteId = $groupedWebsites[$entityWebsiteId];
+                $entitiesByWebsites[$generalWebsiteId][$entity->getId()] = $entity->getId();
+            }
+        }
+
+        return $entitiesByWebsites;
+    }
+}
---
+++ Synchronize/Entity/DevideEntityByWebsiteOrg/Customer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace TNW\Salesforce\Synchronize\Entity\DevideEntityByWebsiteOrg;
+
+use TNW\Salesforce\Synchronize\Entity\DevideEntityByWebsiteOrg;
+
+class Customer extends DevideEntityByWebsiteOrg
+{
+    /**
+     * @var \Magento\Customer\Model\ResourceModel\Customer\Collection
+     */
+    protected $collection;
+
+    /**
+     * Customer constructor.
+     * @param \TNW\Salesforce\Model\Config $config
+     * @param \Magento\Customer\Model\ResourceModel\Customer\Collection $collection
+     */
+    public function __construct(
+        \TNW\Salesforce\Model\Config $config,
+        \Magento\Customer\Model\ResourceModel\Customer\Collection $collection
+    )
+    {
+        $this->collection = $collection;
+        parent::__construct($config);
+    }
+
+
+    /**
+     * @param $ids
+     * @return \Magento\Customer\Model\ResourceModel\Customer\Collection|\Magento\Framework\Data\Collection\AbstractDb
+     */
+    public function loadEntities($ids)
+    {
+        $entities = $this->collection->addFieldToFilter($this->collection->getRowIdFieldName(), $ids);
+
+        return $entities;
+    }
+
+    /**
+     * @param $entity \Magento\Customer\Model\Customer
+     *
+     * @return array
+     */
+    public function getEntityWebsiteIds($entity)
+    {
+        return [$entity->getWebsiteId()];
+    }
+
+}
