47bc37b0ceac064fb211d7d09b995c9a14ee08dc
Evgenii Kalashnikov <e.kalashnikov@prola.ru>
2019-03-22 17:52:44 +0400

    PCMII-579: Fix

--- Model/ResourceModel/Queue/Collection.php
+++ Model/ResourceModel/Queue/Collection.php
@@ -9,6 +9,11 @@ use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 class Collection extends AbstractCollection
 {
     /**
+     * @var string
+     */
+    protected $_idFieldName = 'queue_id';
+
+    /**
      * Resource initialization
      *
      * @return void
