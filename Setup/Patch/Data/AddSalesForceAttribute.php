2bf44d4f0160c4cc249ee1502737de894796dff6
dmitry-mironov <aferseo@gmail.com>
2019-09-18 13:55:34 +0300

    #560 : Create declaration db scheme.Rewrite data install script with Patch script data. | PCMII-560
    Signed-off-by: dmitry-mironov <aferseo@gmail.com>

---
+++ Setup/Patch/Data/AddSalesForceAttribute.php
@@ -0,0 +1,286 @@
+<?php
+
+namespace TNW\Salesforce\Setup\Patch\Data;
+
+use Magento\Customer\Model\Customer;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Module\ModuleResource;
+
+class AddSalesForceAttribute implements DataPatchInterface
+{
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $_moduleDataSetup;
+
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $_eavSetupFactory;
+    /**
+     * @var ModuleResource
+     */
+    private $_moduleResource;
+
+    /**
+     *
+     * {@inheritdoc}
+     * AddSalesForceAttribute constructor.
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param EavSetupFactory $eavSetupFactory
+     * @param ModuleResource $resource
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory,
+        ModuleResource $resource
+    ) {
+        $this->_moduleDataSetup = $moduleDataSetup;
+        $this->_eavSetupFactory = $eavSetupFactory;
+        $this->_moduleResource = $resource;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return array|string[]
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return array|string[]
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return DataPatchInterface|void
+     */
+    public function apply()
+    {
+
+        //if we already have some version in db we don't need to execute this
+        if (version_compare(
+            $this->getVersion(),
+            $this->_moduleResource->getDataVersion('TNW_Salesforce'),
+            '<'
+        )
+        ) {
+            return;
+        }
+
+        $eavSetup = $this->_eavSetupFactory->create(
+            ['setup' => $this->_moduleDataSetup]
+        );
+
+        $eavSetup->addAttribute(
+            Customer::ENTITY,
+            'sforce_id',
+            [
+                'type' => 'varchar',
+                'required' => false,
+                'sort_order' => 1,
+                'visible' => true,
+                'system' => false,
+                'group' => 'Account Information',
+                'default' => null,
+                'label' => 'Salesforce Contact Id'
+            ]
+        );
+
+        $eavSetup->addAttribute(
+            Customer::ENTITY,
+            'sforce_account_id',
+            [
+                'type' => 'varchar',
+                'required' => false,
+                'sort_order' => 2,
+                'visible' => true,
+                'system' => false,
+                'group' => 'Account Information',
+                'default' => null,
+                'label' => 'Salesforce Account Id'
+            ]
+        );
+
+        $eavSetup->addAttribute(
+            Customer::ENTITY,
+            'sync_status',
+            [
+                'type' => 'int',
+                'required' => false,
+                'sort_order' => 3,
+                'visible' => true,
+                'system' => false,
+                'group' => 'Account Information',
+                'default' => 0,
+                'label' => 'Sync Status'
+            ]
+        );
+
+        //prepare default mapping for customer to contact
+        $customerToContact = [
+            [   'magento_attribute_name'=>'email',
+                'salesforce_attribute_name'=>'Email',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'firstname',
+                'salesforce_attribute_name'=>'FirstName',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'lastname',
+                'salesforce_attribute_name'=>'LastName',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'dob',
+                'salesforce_attribute_name'=>'Birthdate',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ]
+        ];
+
+        foreach ($customerToContact as $magento_attr) {
+            $attribute = $eavSetup->getAttribute(
+                Customer::ENTITY,
+                $magento_attr['magento_attribute_name']
+            );
+            $magento_attr['attribute_id'] = $attribute['attribute_id'];
+            $magento_attr['magento_entity_type'] = 'customer';
+            $this->_moduleDataSetup->getConnection()->insert(
+                $this->_moduleDataSetup->getTable('tnw_salesforce_mapper'),
+                $magento_attr
+            );
+        }
+
+        // Customer Address to Account and Contact
+        $customerShippingToContact = [
+            [   'magento_attribute_name'=>'street',
+                'salesforce_attribute_name'=>'MailingStreet',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'city',
+                'salesforce_attribute_name'=>'MailingCity',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'region',
+                'salesforce_attribute_name'=>'MailingState',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'postcode',
+                'salesforce_attribute_name'=>'MailingPostalCode',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'country_id',
+                'salesforce_attribute_name'=>'MailingCountry',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'telephone',
+                'salesforce_attribute_name'=>'Phone',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ]
+        ];
+
+        foreach ($customerShippingToContact as $magento_attr) {
+            $attribute = $eavSetup->getAttribute(
+                'customer_address',
+                $magento_attr['magento_attribute_name']
+            );
+            $magento_attr['magento_entity_type'] = 'customer_address/shipping';
+            $magento_attr['attribute_id'] = $attribute['attribute_id'];
+            $this->_moduleDataSetup->getConnection()->insert(
+                $this->_moduleDataSetup->getTable('tnw_salesforce_mapper'),
+                $magento_attr
+            );
+        }
+
+        /**
+         *
+         * @var $customerBillingToContact
+         */
+        $customerBillingToContact = [
+            [   'magento_attribute_name'=>'company',
+                'salesforce_attribute_name'=>'Name',
+                'attribute_type' => 'string',
+                'object_type' => 'Account'
+            ],
+            [   'magento_attribute_name'=>'street',
+                'salesforce_attribute_name'=>'OtherStreet',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'city',
+                'salesforce_attribute_name'=>'OtherCity',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'region',
+                'salesforce_attribute_name'=>'OtherState',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'postcode',
+                'salesforce_attribute_name'=>'OtherPostalCode',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'country_id',
+                'salesforce_attribute_name'=>'OtherCountry',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ],
+            [   'magento_attribute_name'=>'telephone',
+                'salesforce_attribute_name'=>'OtherPhone',
+                'attribute_type' => 'string',
+                'object_type' => 'Contact'
+            ]
+        ];
+
+        foreach ($customerBillingToContact as $magento_attr) {
+
+            $attribute = $eavSetup->getAttribute(
+                'customer_address',
+                $magento_attr['magento_attribute_name']
+            );
+            $magento_attr['attribute_id'] = $attribute['attribute_id'];
+            $magento_attr['magento_entity_type'] = 'customer_address/billing';
+            $this->_moduleDataSetup->getConnection()->insert(
+                $this->_moduleDataSetup->getTable('tnw_salesforce_mapper'),
+                $magento_attr
+            );
+        }
+    }
+
+    /**
+     *
+     * @return string
+     */
+    private function getVersion()
+    {
+        return '0.0.1';
+    }
+}
