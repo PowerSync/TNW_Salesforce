46643c9fdf8cbd7c53b05f3996a7a35ac473c672
Saravanan Rajagopal <saravanan.r@clsslabs.com>
2019-09-26 18:37:16 +0530

    PCMII-689: Test connection without saving config

---
+++ Plugin/ValidatorSkip.php
@@ -0,0 +1,32 @@
+<?php
+namespace TNW\Salesforce\Plugin;
+
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ActionInterface;
+
+/**
+ * Class ValidatorSkip
+ * @package TNW\Salesforce\Plugin
+ */
+class ValidatorSkip
+{
+    /**
+     * @param \Magento\Framework\App\Request\CsrfValidator $subject
+     * @param \Closure $proceed
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\Framework\App\ActionInterface $action
+     */
+    public function aroundValidate(
+        \Magento\Backend\App\Request\BackendValidator $subject,
+        \Closure $proceed,
+        RequestInterface $request,
+        ActionInterface $action
+    ) {
+        $current_module = $request->getModuleName();
+        if($current_module == "tnw_salesforce"){
+            return false;
+        }
+        $result = $proceed($request, $action);
+        return $result;
+    }
+}
