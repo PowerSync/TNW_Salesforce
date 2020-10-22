439e113d3518c60c3642d946c2e5beb08ae484c3
eermolaev <eermolaev@yandex.ru>
2020-10-22 22:27:16 +0400

    141 Dlownload log fix
    Signed-off-by: eermolaev <eermolaev@yandex.ru>

--- Controller/Adminhtml/System/Log/Download.php
+++ Controller/Adminhtml/System/Log/Download.php
@@ -1,36 +1,54 @@
 <?php
 namespace TNW\Salesforce\Controller\Adminhtml\System\Log;
 
+use Exception;
+use InvalidArgumentException;
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
 use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\Response\Http\FileFactory;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\NotFoundException;
+use TNW\Salesforce\Model\Config;
 
-class Download extends \Magento\Backend\App\Action
+class Download extends Action
 {
     /**
-     * @var \Magento\Framework\App\Response\Http\FileFactory
+     * @var FileFactory
      */
     protected $fileFactory;
 
+    /** @var Config  */
+    protected $salesforceConfig;
+
     public function __construct(
-        \Magento\Backend\App\Action\Context $context,
-        \Magento\Framework\App\Response\Http\FileFactory $fileFactory
+        Context $context,
+        FileFactory $fileFactory,
+        Config $salesforceConfig
     ) {
         $this->fileFactory = $fileFactory;
+        $this->salesforceConfig = $salesforceConfig;
         parent::__construct($context);
     }
 
     /**
      * Dispatch request
      *
-     * @return \Magento\Framework\Controller\ResultInterface|\Magento\Framework\App\ResponseInterface
-     * @throws \Magento\Framework\Exception\NotFoundException
-     * @throws \InvalidArgumentException
-     * @throws \Exception
+     * @return ResultInterface|ResponseInterface
+     * @throws NotFoundException
+     * @throws InvalidArgumentException
+     * @throws Exception
      */
     public function execute()
     {
+        $baseDay = $this->salesforceConfig->logBaseDay();
+
+        $filenamePath = sprintf('log/sforce/%d_%d_%d.log', date('Y'), $baseDay, floor((date('z') + 1) / $baseDay));
+
         return $this->fileFactory->create('sforce.log', [
             'type'  => 'filename',
-            'value' => 'log/sforce.log'
+            'value' => $filenamePath
         ], DirectoryList::VAR_DIR);
     }
 }
