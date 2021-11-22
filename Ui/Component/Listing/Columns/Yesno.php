12fdfbd865a9e828b36a38686147db5c97a1a41b
Evgeniy Ermolaev <eermolaev@yandex.ru>
2021-11-23 01:53:53 +0400

    EL-MSF-00012 Adding B2B Quotes to queue, not adding all to queue
    Signed-off-by: Evgeniy Ermolaev <eermolaev@yandex.ru>

---
+++ Ui/Component/Listing/Columns/Yesno.php
@@ -0,0 +1,61 @@
+<?php
+namespace TNW\Salesforce\Ui\Component\Listing\Columns;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+/**
+ * Class SyncStatus
+ * @package TNW\Salesforce\Ui\Component\Listing\Columns
+ */
+class Yesno extends Column
+{
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            $fieldName = $this->getData('name');
+            foreach ($dataSource['data']['items'] as & $item) {
+                if (isset($item[$fieldName])) {
+                    $item[$fieldName] = $this->setStatusLabel($item[$fieldName]);
+                }
+            }
+        }
+
+        return $dataSource;
+    }
+
+    /**
+     * Set status label
+     *
+     * @param int $key
+     * @return string
+     */
+    protected function setStatusLabel($key)
+    {
+        $labels = $this->getOptions()->toArray();
+
+        return $labels[$key];
+    }
+}
