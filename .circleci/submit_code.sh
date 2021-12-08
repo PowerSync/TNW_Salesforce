35642cd36dac5bf611e878fa7d19f3da213d2ebd
Pavel Bystritsky <p.bystritskyi@gmail.com>
2021-12-08 17:36:13 +0200

    TNW-MSF-00096: Automatic submission to Magento Marketplace (meta package).

--- .circleci/submit_code.sh
+++ .circleci/submit_code.sh
@@ -1,11 +1,17 @@
 #set -x
-cd ..
 
-VERSION=$(grep -o '^ *"version": *"[0-9\.]*"' composer.json|awk '{print $2}'|sed -e 's/"\(.*\)"/\1/g')
+VERSION=$(grep -o '^ *"version": *"[0-9\.]*"' ../composer.json|awk '{print $2}'|sed -e 's/"\(.*\)"/\1/g')
+
+# Create update payload
+ARCH_NAME=code.zip
+SAMPLE=metapackage/composer.json.sample
+TARGET=metapackage/composer.json
+cp $SAMPLE $TARGET
+sed -i "s/\$VERSION/$VERSION/" $TARGET
+zip -j $ARCH_NAME $TARGET
 
 # Upload update to Marketplace
-zip -r code.zip ./*
-git clone git@github.com:PowerSync/TNW_EQP.git --branch main
-mv code.zip TNW_EQP
-cd TNW_EQP
-bin/main code.zip $VERSION
+git clone git@github.com:PowerSync/TNW_EQP.git eqp --branch main
+mv $ARCH_NAME eqp
+cd eqp
+bin/main $ARCH_NAME $VERSION
