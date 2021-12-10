aed0cb3f91c5831069fecf1a0080111732ace281
Pavel Bystritsky <p.bystritskyi@gmail.com>
2021-12-10 18:31:42 +0200

    Meta and shared packages upload added.

--- .circleci/submit_code.sh
+++ .circleci/submit_code.sh
@@ -1,17 +1,30 @@
 #set -x
 
 VERSION=$(grep -o '^ *"version": *"[0-9\.]*"' ../composer.json|awk '{print $2}'|sed -e 's/"\(.*\)"/\1/g')
+WD=$(pwd)
 
-# Create update payload
-ARCH_NAME=code.zip
+# Create shared package update payload
+SHARED_ARCH_NAME=code_shared.zip
+cd ..
+zip -r $SHARED_ARCH_NAME ./*
+
+# Upload shared package update to Marketplace
+cd WD
+git clone git@github.com:PowerSync/TNW_EQP.git eqp --branch main
+mv ../$SHARED_ARCH_NAME eqp
+cd eqp
+bin/main $SHARED_ARCH_NAME $VERSION 0
+
+# Create meta package update payload
+cd $WD
+META_ARCH_NAME=code_meta.zip
 SAMPLE=metapackage/composer.json.sample
 TARGET=metapackage/composer.json
 cp $SAMPLE $TARGET
 sed -i "s/\$VERSION/$VERSION/" $TARGET
-zip -j $ARCH_NAME $TARGET
+zip -j $META_ARCH_NAME $TARGET
 
 # Upload update to Marketplace
-git clone git@github.com:PowerSync/TNW_EQP.git eqp --branch main
-mv $ARCH_NAME eqp
+mv $META_ARCH_NAME eqp
 cd eqp
-bin/main $ARCH_NAME $VERSION
+bin/main $META_ARCH_NAME $VERSION 1
