c276543786cf203efc29d2490e0fcf90a145eabe
andriy.smuzhanistya <andrii.smuzhanitsya@gmail.com>
2022-03-08 09:01:19 +0200

    TNW-145: Customers from not selected customer groups are synchronized

--- Synchronize/Entity/DivideEntityByWebsiteOrg/Customer.php
+++ Synchronize/Entity/DivideEntityByWebsiteOrg/Customer.php
@@ -2,36 +2,65 @@
 
 namespace TNW\Salesforce\Synchronize\Entity\DivideEntityByWebsiteOrg;
 
+use Magento\Customer\Model\ResourceModel\Customer\Collection;
+use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
+use TNW\Salesforce\Model\Config;
+use TNW\Salesforce\Model\Customer\Config as CustomerConfig;
 use TNW\Salesforce\Synchronize\Entity\DivideEntityByWebsiteOrg;
 
 class Customer extends DivideEntityByWebsiteOrg
 {
     /**
-     * @var \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory
+     * @var CollectionFactory
      */
     private $collectionFactory;
 
+    private CustomerConfig $customerConfig;
+
+    private StoreManagerInterface $storeManager;
+
     /**
      * Customer constructor.
-     * @param \TNW\Salesforce\Model\Config $config
-     * @param \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $collectionFactory
+     *
+     * @param Config                $config
+     * @param CollectionFactory     $collectionFactory
+     * @param CustomerConfig        $customerConfig
+     * @param StoreManagerInterface $storeManager
      */
     public function __construct(
-        \TNW\Salesforce\Model\Config $config,
-        \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $collectionFactory
+        Config $config,
+        CollectionFactory $collectionFactory,
+        CustomerConfig $customerConfig,
+        StoreManagerInterface $storeManager
     ) {
         $this->collectionFactory = $collectionFactory;
         parent::__construct($config);
+        $this->customerConfig = $customerConfig;
+        $this->storeManager = $storeManager;
     }
 
     /**
+     * Load customer entities
+     *
      * @param array $ids
-     * @return \Magento\Customer\Model\ResourceModel\Customer\Collection
+     *
+     * @return Collection
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
      */
     public function loadEntities($ids)
     {
         $collection = $this->collectionFactory->create();
         $collection->addFieldToFilter($collection->getRowIdFieldName(), $ids);
+        $websiteId = $this->storeManager->getStore()->getWebsiteId();
+        $useAllGroups = $this->customerConfig->getCustomerAllGroups($websiteId);
+        if (!$useAllGroups) {
+            $customerSyncGroupsIds = $this->customerConfig->getCustomerSyncGroups($websiteId);
+            $customerSyncGroupsIds && $collection->addAttributeToFilter('group_id', ['in' => $customerSyncGroupsIds]);
+        }
 
         return $collection;
     }
