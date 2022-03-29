5dd542f91c753e7a8662bee96069ff7691f5d62d
Ivan Prodanets <prodanecivan@gmail.com>
2022-03-29 17:39:26 +0300

    TNW-205 - inappropriate error message appears when try to change Website for System mapping

--- Model/ResourceModel/Mapper.php
+++ Model/ResourceModel/Mapper.php
@@ -15,4 +15,30 @@ class Mapper extends AbstractDb
     {
         $this->_init('tnw_salesforce_mapper', 'map_id');
     }
+
+    /**
+     * @inheritDoc
+     */
+    protected function _initUniqueFields()
+    {
+        $this->_uniqueFields = [[
+            'field' => [
+                'object_type',
+                'magento_entity_type',
+                'magento_attribute_name',
+                'salesforce_attribute_name',
+                'website_id'
+            ],
+            'title' => __(
+                'Mapper with for such %1, %2, %3, %4, %5',
+                __('Object Type'),
+                __('Magento Object'),
+                __('Magento Attribute'),
+                __('Magento Attribute'),
+                __('Website')
+            )
+        ]];
+
+        return $this;
+    }
 }
