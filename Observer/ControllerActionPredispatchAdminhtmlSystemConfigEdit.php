6c540e163d27f94477c690f4be4e048c2a7d0ade
Ivan Prodanets <prodanecivan@gmail.com>
2022-04-06 17:40:00 +0300

    TNW-117 - Returns Configuration page is available when Salesforce integration is disabled

--- Observer/ControllerActionPredispatchAdminhtmlSystemConfigEdit.php
+++ Observer/ControllerActionPredispatchAdminhtmlSystemConfigEdit.php
@@ -1,50 +1,43 @@
 <?php
 namespace TNW\Salesforce\Observer;
 
+use Exception;
+use Magento\Config\Controller\Adminhtml\System\Config\Edit;
+use Magento\Framework\App\Request\Http;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\UrlInterface;
+use SoapFault;
+use TNW\Salesforce\Client\Salesforce;
 
 /**
  * Predispatch Observer
  */
-class ControllerActionPredispatchAdminhtmlSystemConfigEdit implements \Magento\Framework\Event\ObserverInterface
+class ControllerActionPredispatchAdminhtmlSystemConfigEdit implements ObserverInterface
 {
-    /**
-     * @var \Magento\Framework\UrlInterface
-     */
+    private const TNW_SECTION_PREFIX = 'tnwsforce_';
+    private const TNW_GENERAL_SECTION = 'tnwsforce_general';
+
+    /** @var UrlInterface */
     private $urlBuilder;
 
-    /**
-     * @var \Magento\Framework\Message\ManagerInterface
-     */
+    /** @var ManagerInterface */
     private $messageManager;
 
-    /**
-     * @var \TNW\Salesforce\Client\Salesforce
-     */
+    /** @var Salesforce */
     private $salesforceClient;
 
     /**
-     * @var array
-     */
-    protected $allowSection = [
-        'tnwsforce_customer',
-        'tnwsforce_product',
-        'tnwsforce_order',
-        'tnwsforce_invoice',
-        'tnwsforce_shipment',
-    ];
-
-    /**
-     * ControllerActionPredispatchAdminhtmlSystemConfigEdit constructor.
-     * @param \Magento\Framework\UrlInterface $urlBuilder
-     * @param \Magento\Framework\Message\ManagerInterface $messageManager
-     * @param \TNW\Salesforce\Client\Salesforce $salesforceClient
+     * @param UrlInterface     $urlBuilder
+     * @param ManagerInterface $messageManager
+     * @param Salesforce       $salesforceClient
      */
     public function __construct(
-        \Magento\Framework\UrlInterface $urlBuilder,
-        \Magento\Framework\Message\ManagerInterface $messageManager,
-        \TNW\Salesforce\Client\Salesforce $salesforceClient
+        UrlInterface $urlBuilder,
+        ManagerInterface $messageManager,
+        Salesforce $salesforceClient
     ) {
         $this->urlBuilder = $urlBuilder;
         $this->messageManager = $messageManager;
@@ -53,29 +46,31 @@ class ControllerActionPredispatchAdminhtmlSystemConfigEdit implements \Magento\F
 
     /**
      * @param Observer $observer
+     *
      * @return void
      */
     public function execute(Observer $observer)
     {
-        /** @var \Magento\Framework\App\Request\Http $request */
+        /** @var Http $request */
         $request = $observer->getEvent()->getData('request');
-        if (!in_array($request->getParam('section'), $this->allowSection)) {
+        if (!$this->isAllowedSection((string)$request->getParam('section'))) {
             return;
         }
 
-        /** @var \Magento\Config\Controller\Adminhtml\System\Config\Edit $controllerAction */
+        /** @var Edit $controllerAction */
         $controllerAction = $observer->getEvent()->getData('controller_action');
 
         if (!$this->salesforceClient->getClientStatus()) {
             $this->messageManager->addWarningMessage('Saleseforce Integration is disable');
             $this->redirect($controllerAction);
+
             return;
         }
 
         try {
             $client = $this->salesforceClient->getClient();
             $client->getUserInfo();
-        } catch (\SoapFault $e) {
+        } catch (SoapFault $e) {
             switch (true) {
                 case strcasecmp($e->faultcode, 'sf:INVALID_OPERATION_WITH_EXPIRED_PASSWORD') === 0:
                     $this->messageManager->addErrorMessage(__('You Salesforce password is expired. Login to Salesforce, update password. Put new Password and token in our module configuration.'));
@@ -95,8 +90,9 @@ class ControllerActionPredispatchAdminhtmlSystemConfigEdit implements \Magento\F
             }
 
             $this->redirect($controllerAction);
+
             return;
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             $this->messageManager->addExceptionMessage($e);
             $this->redirect($controllerAction);
         }
@@ -107,10 +103,21 @@ class ControllerActionPredispatchAdminhtmlSystemConfigEdit implements \Magento\F
      */
     protected function redirect(Action $action)
     {
-        $action->getActionFlag()->set('', \Magento\Config\Controller\Adminhtml\System\Config\Edit::FLAG_NO_DISPATCH, true);
+        $action->getActionFlag()->set('', Edit::FLAG_NO_DISPATCH, true);
 
-        /** @var \Magento\Framework\App\Response\Http $response */
         $response = $action->getResponse();
-        $response->setRedirect($this->urlBuilder->getUrl('adminhtml/system_config/edit', ['section'=>'tnwsforce_general']));
+        $url = $this->urlBuilder->getUrl('adminhtml/system_config/edit', ['section'=>'tnwsforce_general']);
+        $response->setRedirect($url);
+    }
+
+    /**
+     * @param string $sectionName
+     *
+     * @return bool
+     */
+    private function isAllowedSection(string $sectionName): bool
+    {
+        return (strpos($sectionName, self::TNW_SECTION_PREFIX) !== false)
+            && $sectionName !== self::TNW_GENERAL_SECTION;
     }
 }
