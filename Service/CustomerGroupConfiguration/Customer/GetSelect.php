aa349ce0f4ee5a2a458cd92c5d58eab845717566
Ivan Prodanets <prodanecivan@gmail.com>
2022-04-18 14:49:22 +0300

    TNW-515 - Guest order from the second website is synced if only Default (General) group is allowed for this website

--- Service/CustomerGroupConfiguration/Customer/GetSelect.php
+++ Service/CustomerGroupConfiguration/Customer/GetSelect.php
@@ -6,7 +6,6 @@ namespace TNW\Salesforce\Service\CustomerGroupConfiguration\Customer;
 use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory;
 use Magento\Framework\DB\Select;
 use TNW\Salesforce\Api\Service\GetSelectInterface;
-use TNW\Salesforce\Service\CustomerGroupConfiguration\GetCustomerGroupIds;
 
 /**
  *  Customer ids filtered by customer group from store configuration
@@ -16,19 +15,12 @@ class GetSelect implements GetSelectInterface
     /** @var CollectionFactory */
     private $collectionFactory;
 
-    /** @var GetCustomerGroupIds */
-    private $getCustomerGroupIds;
-
     /**
      * @param CollectionFactory   $collectionFactory
-     * @param GetCustomerGroupIds $getCustomerGroupIds
      */
-    public function __construct(
-        CollectionFactory     $collectionFactory,
-        GetCustomerGroupIds $getCustomerGroupIds
-    ) {
+    public function __construct(CollectionFactory $collectionFactory)
+    {
         $this->collectionFactory = $collectionFactory;
-        $this->getCustomerGroupIds = $getCustomerGroupIds;
     }
 
     /**
@@ -38,13 +30,13 @@ class GetSelect implements GetSelectInterface
     {
         $collection = $this->collectionFactory->create();
         $collection->addFieldToFilter($collection->getRowIdFieldName(), $entityIds);
-        $customerSyncGroupsIds = $this->getCustomerGroupIds->execute();
-        $customerSyncGroupsIds !== null && $collection->addAttributeToFilter('group_id', ['in' => $customerSyncGroupsIds]);
         $select = $collection->getSelect();
         $select->reset(Select::COLUMNS);
         $select->columns(
             [
-                $collection->getRowIdFieldName()
+                $collection->getRowIdFieldName() => 'entity_id',
+                'group_id',
+                'website_id'
             ]
         );
 
