b03cc3c11a4c482b72248d3053addd2dd476dfdb
andriy.smuzhanistya <andrii.smuzhanitsya@gmail.com>
2022-04-27 10:00:22 +0300

    TNW-615: Update file copyrights/strict_types

--- Synchronize/Transport/Calls/Upsert/Transport/Input.php
+++ Synchronize/Transport/Calls/Upsert/Transport/Input.php
@@ -1,4 +1,8 @@
-<?php
+<?php declare(strict_types=1);
+/**
+ * Copyright © 2022 TechNWeb, Inc. All rights reserved.
+ * See TNW_LICENSE.txt for license details.
+ */
 namespace TNW\Salesforce\Synchronize\Transport\Calls\Upsert\Transport;
 
 /**
