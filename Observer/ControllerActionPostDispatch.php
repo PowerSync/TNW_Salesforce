c81b1b54df056323908a4ed5051356981a0f0224
Evgeniy Ermolaev <eermolaev@yandex.ru>
2022-07-05 00:47:57 +0400

    CABN-33 - Sync disable config doesn't take affect to cron jobs
    Signed-off-by: Evgeniy Ermolaev <eermolaev@yandex.ru>

--- Observer/ControllerActionPostDispatch.php
+++ Observer/ControllerActionPostDispatch.php
@@ -2,6 +2,7 @@
 namespace TNW\Salesforce\Observer;
 
 use Magento\Framework\Event\Observer;
+use TNW\Salesforce\Model\Config;
 
 /**
  * Controller Action Post Dispatch
@@ -14,13 +15,21 @@ class ControllerActionPostDispatch implements \Magento\Framework\Event\ObserverI
     private $eventManager;
 
     /**
+     * @var Config
+     */
+    protected $salesforceConfig;
+
+    /**
      * ControllerActionPostDispatch constructor.
      * @param \Magento\Framework\Event\Manager $eventManager
+     * @param Config $salesforceConfig
      */
     public function __construct(
-        \Magento\Framework\Event\Manager $eventManager
+        \Magento\Framework\Event\Manager $eventManager,
+        Config $salesforceConfig
     ) {
         $this->eventManager = $eventManager;
+        $this->salesforceConfig = $salesforceConfig;
     }
 
     /**
@@ -31,6 +40,10 @@ class ControllerActionPostDispatch implements \Magento\Framework\Event\ObserverI
      */
     public function execute(Observer $observer)
     {
+        if (!$this->salesforceConfig->getSalesforceStatus()) {
+            return;
+        }
+
         $this->eventManager->dispatch('tnw_salesforce_entities_sync');
     }
 }
