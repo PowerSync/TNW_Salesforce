fa427e79b338af171db0b8bb56a9de1c8cb53ebf
andriy.smuzhanistya <andrii.smuzhanitsya@gmail.com>
2022-11-11 10:56:39 +0200

    TNW-1649: [DB Profile] ProcessQueue Consumer. Load customer attributes (For 2000 customers - 4000 times)

--- Service/Company/GenerateCompanyName.php
+++ Service/Company/GenerateCompanyName.php
@@ -6,7 +6,9 @@
 
 namespace TNW\Salesforce\Service\Company;
 
+use Magento\Framework\Exception\LocalizedException;
 use TNW\Salesforce\Api\Service\Company\GenerateCompanyNameInterface;
+use TNW\Salesforce\Service\Synchronize\Unit\Load\GetCustomerAddressesByAddressIds;
 use TNW\Salesforce\Utils\Company;
 
 /**
@@ -14,32 +16,62 @@ use TNW\Salesforce\Utils\Company;
  */
 class GenerateCompanyName implements GenerateCompanyNameInterface
 {
+    /** @var GetCustomerAddressesByAddressIds */
+    private $getCustomerAddressesByAddressIds;
+
+    /**
+     * @param GetCustomerAddressesByAddressIds $getCustomerAddressesByAddressIds
+     */
+    public function __construct(
+        GetCustomerAddressesByAddressIds $getCustomerAddressesByAddressIds
+    ) {
+        $this->getCustomerAddressesByAddressIds = $getCustomerAddressesByAddressIds;
+    }
+
     /**
      * @inheritDoc
      */
     public function execute($customer): string
     {
         $customerCompany = trim((string)$customer->getCompany());
-        $billingAddress = $customer->getDefaultBillingAddress();
-        $shippingAddress = $customer->getDefaultShippingAddress();
-        switch (true) {
-            case (!empty($customerCompany)):
-                $company = $customerCompany;
-                break;
-
-            case ($billingAddress && !empty(trim((string)$billingAddress->getCompany()))):
+        if (!empty($customerCompany)) {
+            $company = $customerCompany;
+        } else {
+            $billingAddress = $this->getAddressByType($customer, 'default_billing');
+            if ($billingAddress) {
                 $company = trim((string)$billingAddress->getCompany());
-                break;
+            }
 
-            case ($shippingAddress && !empty(trim((string)$shippingAddress->getCompany()))):
-                $company = trim((string)$shippingAddress->getCompany());
-                break;
+            if (empty($company)) {
+                $shippingAddress = $this->getAddressByType($customer, 'default_shipping');
+                if ($shippingAddress) {
+                    $company = trim((string)$shippingAddress->getCompany());
+                }
+            }
 
-            default:
+            if (empty($company)) {
                 $company = Company::generateCompanyByCustomer($customer);
-                break;
+            }
         }
 
         return $company;
+    }
+
+    /**
+     * @param        $customer
+     * @param string $type
+     *
+     * @return mixed|null
+     * @throws LocalizedException
+     */
+    private function getAddressByType($customer, string $type)
+    {
+        $address = null;
+        $addressId = $customer->getData($type);
+        if ($addressId) {
+            $address = $this->getCustomerAddressesByAddressIds->execute([$addressId])[$addressId] ?? null;
+        }
+
+        return $address;
     }
 }
